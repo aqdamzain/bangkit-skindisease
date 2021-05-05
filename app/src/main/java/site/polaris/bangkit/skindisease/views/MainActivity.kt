a8e05925255af4c6fd8d92fc2f89fe4ab8a86c9a
aqdamzain <aqdam.zain@ui.ac.id>
2021-05-06 01:18:27 +0700

    create android app

---
+++ app/src/main/java/site/polaris/bangkit/skindisease/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package site.polaris.bangkit.skindisease.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import site.polaris.bangkit.skindisease.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
