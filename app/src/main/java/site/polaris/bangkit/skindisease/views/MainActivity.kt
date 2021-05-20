9e1caa6577611bfa45d9a369aec0e5df13153b79
aqdamzain <aqdam.zain@ui.ac.id>
2021-05-20 11:57:56 +0700

    xml main bottom nav

--- app/src/main/java/site/polaris/bangkit/skindisease/views/MainActivity.kt
+++ app/src/main/java/site/polaris/bangkit/skindisease/views/MainActivity.kt
@@ -2,11 +2,15 @@ package site.polaris.bangkit.skindisease.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import site.polaris.bangkit.skindisease.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
