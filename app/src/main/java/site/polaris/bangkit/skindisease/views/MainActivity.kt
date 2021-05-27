cec3e63d23d33b04dd303aff8609e122efbc38fa
aqdamzain <aqdam.zain@ui.ac.id>
2021-05-27 22:03:09 +0700

    add camera function

--- app/src/main/java/site/polaris/bangkit/skindisease/views/MainActivity.kt
+++ app/src/main/java/site/polaris/bangkit/skindisease/views/MainActivity.kt
@@ -1,7 +1,14 @@
 package site.polaris.bangkit.skindisease.views
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import site.polaris.bangkit.skindisease.R
@@ -10,6 +17,11 @@ import site.polaris.bangkit.skindisease.models.Report
 import site.polaris.bangkit.skindisease.views.adapters.ReportListAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
 
     private lateinit var binding: ActivityMainBinding
     private val list = ArrayList<Report>()
@@ -24,8 +36,11 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.background = null
         bottomNavigationView.menu.getItem(1).isEnabled = false
 
+        populateMenu()
         populateDummy()
         showRecyclerList()
+
+
 
 
     }
@@ -39,6 +54,42 @@ class MainActivity : AppCompatActivity() {
     private fun populateDummy() {
         for (i in 1..10) {
             list.add(Report("test","test"))
+        }
+    }
+
+    private fun populateMenu() {
+        fab_cam.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == CAMERA_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                Toast.makeText(
+                    this,
+                    "Oops you just denied the permission for camera. " +
+                            "Don't worry you can allow it in the settings.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
