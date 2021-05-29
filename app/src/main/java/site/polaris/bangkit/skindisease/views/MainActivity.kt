ab2ca44e271f3b7f4e5f902f0b91ec9302653f58
aqdamzain <aqdam.zain@ui.ac.id>
2021-05-29 16:53:24 +0700

    add info fragment

--- app/src/main/java/site/polaris/bangkit/skindisease/views/MainActivity.kt
+++ app/src/main/java/site/polaris/bangkit/skindisease/views/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_result), drawerLayout)
+                R.id.nav_home, R.id.nav_result, R.id.nav_info), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ app/src/main/java/site/polaris/bangkit/skindisease/views/ui/info/InfoViewModel.kt
@@ -0,0 +1,13 @@
+package site.polaris.bangkit.skindisease.views.ui.info
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class InfoViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is info Fragment"
+    }
+    val text: LiveData<String> = _text
+}
