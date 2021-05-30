b080cfcaffe335cbba5bd21991888bb884053bec
aqdamzain <aqdam.zain@ui.ac.id>
2021-05-30 07:29:58 +0700

    shorten parcelable implementation with parcelize annotation

--- app/src/main/java/site/polaris/bangkit/skindisease/models/Report.kt
+++ app/src/main/java/site/polaris/bangkit/skindisease/models/Report.kt
@@ -1,8 +1,9 @@
 package site.polaris.bangkit.skindisease.models
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Report(
     var title: String?,
     var image: String?,
@@ -10,37 +11,4 @@ data class Report(
     var suggestion: String?,
     var sendDate: String?,
     var resultDate: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(title)
-        parcel.writeString(image)
-        parcel.writeString(description)
-        parcel.writeString(suggestion)
-        parcel.writeString(sendDate)
-        parcel.writeString(resultDate)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Report> {
-        override fun createFromParcel(parcel: Parcel): Report {
-            return Report(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Report?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
