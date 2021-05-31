bdcd1c0fa7c50e6c33875e9d0b108e9cf8bcddab
aqdamzain <aqdam.zain@ui.ac.id>
2021-05-31 13:48:11 +0700

    add room entity

--- app/src/main/java/site/polaris/bangkit/skindisease/models/Report.kt
+++ app/src/main/java/site/polaris/bangkit/skindisease/models/Report.kt
@@ -1,14 +1,33 @@
 package site.polaris.bangkit.skindisease.models
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Entity
 @Parcelize
 data class Report(
-    var title: String?,
-    var image: String?,
-    var description: String?,
-    var suggestion: String?,
-    var sendDate: String?,
-    var resultDate: String?
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+
+    @ColumnInfo(name = "title")
+    var title: String? = null,
+
+    @ColumnInfo(name = "image")
+    var image: String? = null,
+
+    @ColumnInfo(name = "description")
+    var description: String? = null,
+
+    @ColumnInfo(name = "suggestion")
+    var suggestion: String? = null,
+
+    @ColumnInfo(name = "send_date")
+    var sendDate: String? = null,
+
+    @ColumnInfo(name = "result_date")
+    var resultDate: String? =null
 ) : Parcelable
