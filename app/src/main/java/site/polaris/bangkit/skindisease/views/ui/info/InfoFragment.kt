0b3cb4e5d0f03a0bee67bb5c1cab8bb66d7ed6bc
aqdamzain <aqdam.zain@ui.ac.id>
2021-06-03 00:50:49 +0700

    add disclaimer & privacy content to info UI

--- app/src/main/java/site/polaris/bangkit/skindisease/views/ui/info/InfoFragment.kt
+++ app/src/main/java/site/polaris/bangkit/skindisease/views/ui/info/InfoFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import site.polaris.bangkit.skindisease.R
 
 class InfoFragment : Fragment() {
@@ -19,13 +16,6 @@ class InfoFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        infoViewModel =
-                ViewModelProvider(this).get(InfoViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_info, container, false)
-        val textView: TextView = root.findViewById(R.id.text_info)
-        infoViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_info, container, false)
     }
 }
