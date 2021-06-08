2ecdb97c93378e0d94471dc840b1d91f1eb0f0f6
aqdamzain <aqdam.zain@ui.ac.id>
2021-06-08 23:21:46 +0700

    add cropping feature

--- app/src/main/java/site/polaris/bangkit/skindisease/views/ProcessActivity.kt
+++ app/src/main/java/site/polaris/bangkit/skindisease/views/ProcessActivity.kt
@@ -1,12 +1,14 @@
 package site.polaris.bangkit.skindisease.views
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import site.polaris.bangkit.skindisease.Utils.base64ToBitmap
+import site.polaris.bangkit.skindisease.Utils.bitmapToBase64
 import site.polaris.bangkit.skindisease.databinding.ActivityProcessBinding
 import site.polaris.bangkit.skindisease.helper.DateHelper
 import site.polaris.bangkit.skindisease.helper.ViewModelFactory
@@ -24,6 +26,7 @@ class ProcessActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProcessBinding
 
     private var report: Report? = null
+    private var cropped: Bitmap? = null
 
     private lateinit var processViewModel: ProcessViewModel
 
@@ -36,44 +39,57 @@ class ProcessActivity : AppCompatActivity() {
 
         processViewModel = obtainViewModel(this@ProcessActivity)
 
-        binding.ivPreview.setImageBitmap(base64ToBitmap(cameraResult))
+        report = Report()
 
-        binding.cancelButton.visibility = View.VISIBLE
-        binding.sendButton.visibility = View.VISIBLE
+        binding.ivPreview.visibility = View.GONE
+        //binding.ivPreview.setImageBitmap(base64ToBitmap(cameraResult))
+        binding.cropImageView.setImageBitmap(base64ToBitmap(cameraResult))
+
+        binding.cancelButton.visibility = View.GONE
+        binding.sendButton.visibility = View.GONE
         binding.detailButton.visibility = View.GONE
         binding.progressbar.visibility = View.GONE
         binding.tvLoading.visibility= View.GONE
         binding.diseaseName.visibility = View.GONE
+        binding.cropButton.visibility = View.VISIBLE
+
+        binding.cropButton.setOnClickListener{
+            cropped = binding.cropImageView.croppedImage
+            binding.ivPreview.setImageBitmap(cropped)
+            binding.cropImageView.visibility = View.GONE
+            binding.cropButton.visibility = View.GONE
+            binding.ivPreview.visibility = View.VISIBLE
+            binding.cancelButton.visibility = View.VISIBLE
+            binding.sendButton.visibility = View.VISIBLE
+        }
 
         binding.sendButton.setOnClickListener{
             binding.ivPreview.visibility = View.GONE
             binding.progressbar.visibility = View.VISIBLE
             binding.tvLoading.visibility= View.VISIBLE
+            report.let { report ->
+                report?.image = bitmapToBase64(cropped as Bitmap)
+                report?.sendDate = DateHelper.getCurrentDate()
+            }
             processViewModel.postFeatures(ReportRequest(cameraResult)).observe(this,
-                    { response ->
-                        if (response != null) {
-                            binding.diseaseName.text = response.disease
-                            binding.ivPreview.visibility = View.VISIBLE
-                            binding.diseaseName.visibility = View.VISIBLE
-                            binding.cancelButton.visibility = View.GONE
-                            binding.sendButton.visibility = View.GONE
-                            binding.detailButton.visibility = View.VISIBLE
-                            binding.progressbar.visibility = View.GONE
-                            binding.tvLoading.visibility= View.GONE
-                            report?.title = response.disease
-                            report?.description = response.description
-                            report?.suggestion = response.suggestion
-                            report?.resultDate = DateHelper.getCurrentDate()
+                { response ->
+                    if (response != null) {
+                        binding.diseaseName.text = response.disease
+                        binding.ivPreview.visibility = View.VISIBLE
+                        binding.diseaseName.visibility = View.VISIBLE
+                        binding.cancelButton.visibility = View.GONE
+                        binding.sendButton.visibility = View.GONE
+                        binding.detailButton.visibility = View.VISIBLE
+                        binding.progressbar.visibility = View.GONE
+                        binding.tvLoading.visibility = View.GONE
+                        report?.title = response.disease
+                        report?.description = response.description
+                        report?.suggestion = response.suggestion
+                        report?.resultDate = DateHelper.getCurrentDate()
 
-                            processViewModel.insert(report as Report)
-                        }
-                    })
-        }
-        report = Report()
-
-        report.let { report ->
-            report?.image = cameraResult
-            report?.sendDate = DateHelper.getCurrentDate()
+                        processViewModel.insert(report as Report)
+                    }
+                })
         }
 
 
